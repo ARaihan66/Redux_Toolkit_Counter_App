c6f3b872ff64263b765a18804f7e7546f0db81b8
Abu Raihan <aburaihancse066@gmail.com>
2022-12-18 00:58:11 +0600

    Add createSlice

---
+++ src/Services/CounterSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const CounterSlice = createSlice({
+    name: 'counter',
+    initialState: {
+        counter: 0
+    },
+    reducers: {
+        increment: (state) => {
+            state.counter = state.counter + 1
+        },
+        decrement: (state) => {
+            state.counter = state.counter - 1
+        },
+        reset: (state) => {
+            state.counter = 0
+        }
+    }
+
+})
+
+export const { increment, decrement, reset } = CounterSlice.actions;
+
+export default CounterSlice.reducers;
